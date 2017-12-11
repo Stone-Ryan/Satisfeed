c61430edbc9dc95553f0a430d3e58f2b172a232a
SDRyan9 <stonedouglasryan@gmail.com>
2017-12-11 04:26:05 -0800

    added file to gitignore

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App /> 
+  </ Provider>, document.querySelector('#root')
+);
