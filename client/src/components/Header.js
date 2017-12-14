60b87e64b2f5e8ad7a1ed3c32a4c1686ec41d855
SDRyan9 <stonedouglasryan@gmail.com>
2017-12-13 19:55:34 -0800

    added redirects on auth routes, oauth cycle functioning

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Login With Google</a></li>
+        );
+      default:
+        return (<li><a href="/api/logout">Logout</a></li>);
+    }
+  };
+
   render() {
     return(
       <nav>
@@ -8,9 +23,7 @@ class Header extends Component {
             Satisfeed
           </a>
           <ul className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -18,4 +31,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
