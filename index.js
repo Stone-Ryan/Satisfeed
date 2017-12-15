3d129ff568e4d9798618e92244b28ded22df9152
SDRyan9 <stonedouglasryan@gmail.com>
2017-12-15 15:01:32 -0800

    added body-parser dependency and started /api/stripe route in routes/billingRoutes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
 
 require('./models/User');
 require('./services/passport');
@@ -13,6 +14,8 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
+
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 2592000000,
@@ -23,6 +26,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
