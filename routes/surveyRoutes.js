1c6a195ebd711085952743a8313c4cdbf1d25c94
SDRyan9 <stonedouglasryan@gmail.com>
2017-12-15 21:25:11 -0800

    added surveyTemplates and started Malier file and survey routes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../servies/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+
 const Survey = mongoose.model('surveys');
+
+
 
 module.exports = app => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
@@ -14,7 +19,9 @@ module.exports = app => {
       recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
-    })
+    });
+
+    const mailer = new Mailer(survey, surveyTemplate(survey));
   });
 
   // app.get('/api/surveys');
