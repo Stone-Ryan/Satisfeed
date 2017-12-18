ff46112c2e49ab1b982d06f36b4bf08e3632b28e
SDRyan9 <stonedouglasryan@gmail.com>
2017-12-17 19:01:40 -0800

    added send method to Mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -7,6 +7,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email('no-reply@satisfeed.com')
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
@@ -31,12 +32,22 @@ class Mailer extends helper.Mail {
     this.addTrackingSettings(trackingSettings);
   }
 
-  addRecipients(){
+  addRecipients() {
     const personalize = new helper.Personalization();
     this.recipients.forEach(recipient => {
       personalize.addTo(recipient);
     });
     this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+    const response = this.sgApi.API(request);
+    return response;
   }
 }
 
