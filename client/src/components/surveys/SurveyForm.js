74f30110de0675ad8ecc76498cd15a5a71520bb7
SDRyan9 <stonedouglasryan@gmail.com>
2017-12-20 20:06:56 -0800

    installed lodash and refactored renderFields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,33 +1,18 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import _ from 'lodash';
 import SurveyField from './SurveyField';
 
+
+const FIELDS = [
+  {label: 'Survey Title', name: 'title'},
+  {label: 'Subject Line', name: 'subject'},
+  {label: 'Email Body', name:'body'},
+  {label: 'Recipient List', name:'emails'}
+]
 class SurveyForm extends Component {
   renderFields() {
-    return (
-      <div>
-        <Field
-          type="text"
-          name="title"
-          component={SurveyField}
-          label="Survey Title" />
-        <Field
-          type="text"
-          name="subject"
-          component={SurveyField}
-          label="Subject Line" />
-        <Field
-          type="text"
-          name="body"
-          component={SurveyField}
-          label="Email Body" />
-        <Field
-          type="text"
-          name="emails"
-          component={SurveyField}
-          label="Recipient List" />
-      </div>
-    );
+
   }
 
   render() {
