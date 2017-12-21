532fbae780bc663ed18ce290cd8d10a319f39aff
SDRyan9 <stonedouglasryan@gmail.com>
2017-12-20 20:19:47 -0800

    included button stlying and add cancel link to /survey/new

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import _ from 'lodash';
-import SurveyField from './SurveyField';
+import { Link } from 'react-router-dom';
 
+import SurveyField from './SurveyField';
 
 const FIELDS = [
   {label: 'Survey Title', name: 'title'},
@@ -24,7 +25,15 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     )
