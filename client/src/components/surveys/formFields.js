e003b845d7d8ef3c17bddd7acc58c8ecbc187c20
SDRyan9 <stonedouglasryan@gmail.com>
2017-12-22 13:32:22 -0800

    renamed emails in formFields to recipients to match backend naming

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
   {label: 'Survey Title', name: 'title', errMess: ' title'},
   {label: 'Subject Line', name: 'subject', errMess: ' subject'},
   {label: 'Email Body', name:'body', errMess: ' body'},
-  {label: 'Recipient List', name:'emails', errMess: 'n email'}
+  {label: 'Recipient List', name:'recipients', errMess: 'n email'}
 ];
