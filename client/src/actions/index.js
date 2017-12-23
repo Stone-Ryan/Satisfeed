114a31eb56ec26d5ab568ef956bf412bcb79aef7
SDRyan9 <stonedouglasryan@gmail.com>
2017-12-22 17:49:14 -0800

    added fetchSurveys action and reducer and wired reducer to combineReducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user')
@@ -18,6 +18,12 @@ export const submitSurvey = (values, history) => async dispatch => {
   const res = await axios.post('/api/surveys', values);
 
   history.push('/surveys');
-  
+
   dispatch({ type: FETCH_USER, payload: res.data })
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
